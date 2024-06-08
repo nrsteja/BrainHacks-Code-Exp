461f21993ea9b7a9dc7b71cb56e7ea4a1b56d235
nrsteja <tejeswara.rajani@gmail.com>
2024-06-08 13:52:57 +0800

    FigmaCode3

---
+++ NativeHackerApp/app/screens/Admin/EditPage.jsx
@@ -0,0 +1,251 @@
+import * as React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Pressable,
+  Image,
+  StyleSheet,
+} from "react-native";
+
+const ItemComponent = ({ emoji, itemName }) => {
+  const handleIncrement = () => {};
+  const handleDecrement = () => {};
+
+  return (
+    <View style={styles.itemContainer}>
+      <View style={styles.itemDetails}>
+        <View style={styles.emojiContainer}>
+          <Text>{emoji}</Text>
+        </View>
+        <View style={styles.itemName}>
+          <Text>{itemName}</Text>
+        </View>
+      </View>
+      <View style={styles.quantityControls}>
+        <Pressable onPress={handleDecrement} style={styles.controlButton}>
+          <Text>-</Text>
+        </Pressable>
+        <Pressable onPress={handleIncrement} style={styles.controlButton}>
+          <Text>+</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+function MyComponent() {
+  const handleAddItem = () => {};
+  const handleDeleteItem = () => {};
+  const handleView = () => {};
+  const handleUpdate = () => {};
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>GroceryGrabber</Text>
+      </View>
+      <View style={styles.actionRow}>
+        <TouchableOpacity onPress={handleView}>
+          <Text style={styles.actionText}>View</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleUpdate} style={styles.updateButton}>
+          <Text style={styles.updateButtonText}>Update</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.divider} />
+      <View style={styles.actionRow}>
+        <TouchableOpacity onPress={handleAddItem} style={styles.addButton}>
+          <Text>Add Item</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleDeleteItem}
+          style={styles.deleteButton}
+        >
+          <Text>Delete Item</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.itemsContainer}>
+        <View style={styles.itemsHeader}>
+          <Text style={styles.itemsHeaderText}>Items</Text>
+          <Text style={styles.itemsCountText}>5 items</Text>
+        </View>
+        <ItemComponent emoji="🍜" itemName="Gardenia Bread" />
+        <ItemComponent emoji="🍜" itemName="Eggs" />
+        <ItemComponent emoji="🍜" itemName="Milk" />
+        <ItemComponent emoji="🍜" itemName="Peanut butter" />
+        <ItemComponent emoji="🍜" itemName="Dragon Fruit" />
+      </View>
+      <View style={styles.footer}>
+        <View style={styles.footerImageContainer}>
+          <Image
+            source={{
+              uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/0a3a3fe259f6c8ab64003e9294a1c9d48a6cd7609019165cce4a3a33a931cab1?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+            }}
+            style={styles.footerImage}
+          />
+        </View>
+        <Image
+          source={{
+            uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/30e3d31c19bd0f1572ad7b026331c9ca2f1456fef2f7240082d0d7f41419665f?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+          }}
+          style={styles.footerSecondImage}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    width: "100%",
+    maxWidth: 480,
+    alignItems: "center",
+    marginHorizontal: "auto",
+  },
+  header: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+    width: "100%",
+    backgroundColor: "green",
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+  actionRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    width: "100%",
+    marginTop: 16,
+  },
+  actionText: {
+    color: "#4a4a4a",
+    fontSize: 20,
+  },
+  updateButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 24,
+    paddingVertical: 8,
+    backgroundColor: "#6b6b6b",
+    borderRadius: 100,
+  },
+  updateButtonText: {
+    color: "#ececec",
+  },
+  divider: {
+    marginTop: 32,
+    width: "100%",
+    height: 1,
+    backgroundColor: "black",
+  },
+  addButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    backgroundColor: "red",
+    borderRadius: 50,
+  },
+  deleteButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    backgroundColor: "red",
+    borderRadius: 50,
+  },
+  itemsContainer: {
+    width: "100%",
+    maxWidth: 335,
+    marginTop: 16,
+  },
+  itemsHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+  },
+  itemsHeaderText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#6b6b6b",
+  },
+  itemsCountText: {
+    fontSize: 12,
+    color: "#8c8c8c",
+  },
+  itemContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    marginTop: 12,
+    backgroundColor: "#e0e0e0",
+    borderRadius: 16,
+  },
+  itemDetails: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  emojiContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+    backgroundColor: "white",
+    borderRadius: 16,
+    height: 52,
+    width: 52,
+  },
+  itemName: {
+    marginLeft: 8,
+  },
+  quantityControls: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  controlButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+    backgroundColor: "#ff4d4d",
+    height: 20,
+    width: 20,
+    marginHorizontal: 4,
+  },
+  footer: {
+    flexDirection: "row",
+    paddingLeft: 4,
+    paddingRight: 32,
+    paddingVertical: 1,
+    marginTop: 24,
+    width: "100%",
+    backgroundColor: "#1c1c1c",
+    justifyContent: "space-between",
+  },
+  footerImageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+    backgroundColor: "#6b6b6b",
+  },
+  footerImage: {
+    aspectRatio: 1,
+    width: 38,
+  },
+  footerSecondImage: {
+    aspectRatio: 1,
+    width: 36,
+  },
+});
+
+export default MyComponent;
---
+++ NativeHackerApp/app/screens/Admin/Profile.jsx
@@ -0,0 +1,371 @@
+import * as React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Alert,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+
+// Screen width for responsive design
+const screenWidth = Dimensions.get("window").width;
+
+const UserInformation = ({ label, value, iconSrc }) => {
+  return (
+    <TouchableOpacity
+      onPress={() => Alert.alert(`${label}: ${value}`)}
+      style={styles.infoContainer}
+    >
+      <View style={styles.infoContent}>
+        <Image source={{ uri: iconSrc }} style={styles.icon} />
+        <View style={styles.infoTextContainer}>
+          <Text style={styles.infoLabel}>{label}</Text>
+          <Text style={styles.infoValue}>{value}</Text>
+        </View>
+      </View>
+      <Image
+        source={{
+          uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/1674691437b4cb1a5f55cc817b1ed0a10f2b254bf06f5c0ed79a8fa7237237c8?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+        }}
+        style={styles.arrowIcon}
+      />
+    </TouchableOpacity>
+  );
+};
+
+const UserInfoList = ({ data }) => {
+  return (
+    <View>
+      {data.map((info, index) => (
+        <UserInformation key={index} {...info} />
+      ))}
+    </View>
+  );
+};
+
+const Profile = () => {
+  const userInfoData = [
+    {
+      label: "NTUC FairPrice",
+      value: "Parent Company",
+      iconSrc:
+        "https://cdn.builder.io/api/v1/image/assets/TEMP/2cc2b53f924c0d4bfa2b79729f42dc61a94c6e8f1136b8550d5b9bee4517dd54?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+    },
+    {
+      label: "NIN",
+      value: "Identification Verification",
+      iconSrc:
+        "https://cdn.builder.io/api/v1/image/assets/TEMP/adcf512a559427a5b1d7cd1258beff7433bb016d6879219580271ae29495b6f3?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+    },
+    {
+      label: "wdlsFairprice@gmail.com",
+      value: "Email Address",
+      iconSrc:
+        "https://cdn.builder.io/api/v1/image/assets/TEMP/60dc7c3a82e80bc3f1ae72546e8c53320583965252ab1353e7634eac31366608?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>GroceryGrabber</Text>
+      </View>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Profile</Text>
+      </View>
+      <View style={styles.divider} />
+      <View style={styles.shopInfo}>
+        <Image
+          source={{
+            uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/f0536dd54c0fa564069230e9cbe9162cc80f4dad46a57a95c3d280c75ccccaad?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+          }}
+          style={styles.shopIcon}
+        />
+        <View style={styles.shopTextContainer}>
+          <Text style={styles.shopName}>Woodlands FairPrice</Text>
+          <Image
+            source={{
+              uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/81c17d374f558d400eec5bd91bda0cf0f7996f7ba1fb35a18df637cd61bb6b12?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+            }}
+            style={styles.shopArrowIcon}
+          />
+        </View>
+      </View>
+      <View style={styles.userRoleContainer}>
+        <Text style={styles.userRole}>Super User</Text>
+        <Text style={styles.addIcon}>+</Text>
+      </View>
+      <View style={styles.accountInfo}>
+        <Text style={styles.accountText}>Account Number: 043423802</Text>
+        <Image
+          source={{
+            uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/7cda674d4ee51c2bb31952b8b57cb209f7f6ad09235af94ccbce0c6f54a82313?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+          }}
+          style={styles.accountIcon}
+        />
+      </View>
+      <TouchableOpacity
+        style={styles.detailsContainer}
+        onPress={() => Alert.alert("Account Name")}
+      >
+        <View style={styles.detailsContent}>
+          <Image
+            source={{
+              uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/cb3055b52c711c964baebfa14021ffa66175a880d316d6f586c01d444a14d9ef?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+            }}
+            style={styles.icon}
+          />
+          <View style={styles.detailsTextContainer}>
+            <Text style={styles.detailsLabel}>@WoodlandsFairPrice</Text>
+            <Text style={styles.detailsValue}>Account Name</Text>
+          </View>
+        </View>
+        <Image
+          source={{
+            uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/4531e17fc9b9aa9ba5b0d560e89007fc1b42d083d71e81a326dc7798bbffbba9?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+          }}
+          style={styles.arrowIcon}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.detailsContainer}
+        onPress={() => Alert.alert("Address")}
+      >
+        <View style={styles.detailsContent}>
+          <Image
+            source={{
+              uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/1cf565bf3dad06c2be5c57cf4e1c241b1104ecb632b3e951c8dbf3844384274b?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+            }}
+            style={styles.icon}
+          />
+          <View style={styles.detailsTextContainer}>
+            <Text style={styles.detailsLabel}>Woodlands,Singapore</Text>
+            <Text style={styles.detailsValue}>Address</Text>
+          </View>
+        </View>
+        <Image
+          source={{
+            uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/4531e17fc9b9aa9ba5b0d560e89007fc1b42d083d71e81a326dc7798bbffbba9?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+          }}
+          style={styles.arrowIcon}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.detailsContainer}
+        onPress={() => Alert.alert("Phone number")}
+      >
+        <View style={styles.detailsContent}>
+          <Image
+            source={{
+              uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/2713c55ad2ddbad7651a37d20e794db71308b94844ac73c32af76a17a6cd0de6?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+            }}
+            style={styles.icon}
+          />
+          <View style={styles.detailsTextContainer}>
+            <Text style={styles.detailsLabel}>+65 92350345</Text>
+            <Text style={styles.detailsValue}>Phone number</Text>
+          </View>
+        </View>
+        <Image
+          source={{
+            uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/4531e17fc9b9aa9ba5b0d560e89007fc1b42d083d71e81a326dc7798bbffbba9?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+          }}
+          style={styles.arrowIcon}
+        />
+      </TouchableOpacity>
+      <UserInfoList data={userInfoData} />
+      <View style={styles.footer}>
+        <Image
+          source={{
+            uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/0a3a3fe259f6c8ab64003e9294a1c9d48a6cd7609019165cce4a3a33a931cab1?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+          }}
+          style={styles.footerIcon}
+        />
+        <View style={styles.footerButton}>
+          <Image
+            source={{
+              uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/30e3d31c19bd0f1572ad7b026331c9ca2f1456fef2f7240082d0d7f41419665f?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
+            }}
+            style={styles.footerButtonIcon}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    alignSelf: "center",
+    width: screenWidth < 480 ? "100%" : 480,
+  },
+  header: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 16,
+    backgroundColor: "#4B5563",
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+    textAlign: "center",
+  },
+  titleContainer: {
+    alignSelf: "center",
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#111827",
+  },
+  divider: {
+    height: 1,
+    backgroundColor: "#D1D5DB",
+    marginVertical: 10,
+  },
+  shopInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  shopIcon: {
+    width: 50,
+    height: 50,
+  },
+  shopTextContainer: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  shopName: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#1F2937",
+  },
+  shopArrowIcon: {
+    width: 30,
+    height: 30,
+  },
+  userRoleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+  },
+  userRole: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#111827",
+  },
+  addIcon: {
+    fontSize: 20,
+    color: "#FFFFFF",
+  },
+  accountInfo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  accountText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#6B7280",
+  },
+  accountIcon: {
+    width: 25,
+    height: 25,
+  },
+  detailsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 8,
+    marginVertical: 5,
+  },
+  detailsContent: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  detailsTextContainer: {
+    marginLeft: 10,
+  },
+  detailsLabel: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#4338CA",
+  },
+  detailsValue: {
+    fontSize: 12,
+    color: "#6B7280",
+    marginTop: 5,
+  },
+  icon: {
+    width: 25,
+    height: 25,
+  },
+  arrowIcon: {
+    width: 20,
+    height: 20,
+  },
+  infoContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#D1D5DB",
+    marginVertical: 5,
+  },
+  infoContent: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  infoTextContainer: {
+    marginLeft: 10,
+  },
+  infoLabel: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#4338CA",
+  },
+  infoValue: {
+    fontSize: 12,
+    color: "#6B7280",
+    marginTop: 5,
+  },
+  footer: {
+    flexDirection: "row",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    backgroundColor: "#4B5563",
+  },
+  footerIcon: {
+    width: 38,
+    height: 38,
+    alignSelf: "center",
+  },
+  footerButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 10,
+    backgroundColor: "#374151",
+  },
+  footerButtonIcon: {
+    width: 36,
+    height: 36,
+  },
+});
+
+export default Profile;
