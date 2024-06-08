6d206d4f24165cd4a3b4ee1d35013fb33e55bc01
nrsteja <tejeswara.rajani@gmail.com>
2024-06-08 15:59:03 +0800

    Account Page Update

--- NativeHackerApp/app/screens/Account.jsx
+++ NativeHackerApp/app/screens/Account.jsx
@@ -1,122 +1,20 @@
-import * as React from "react";
+import React from "react";
 import {
   View,
-  Image,
   Text,
+  Image,
   TouchableOpacity,
-  Alert,
   StyleSheet,
-  SafeAreaView,
+  ScrollView,
+  Dimensions,
+  Alert,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: "#fff",
-    maxWidth: 480,
-    marginHorizontal: "auto",
-  },
-  header: {
-    justifyContent: "center",
-    alignItems: "center",
-    paddingVertical: 16,
-    width: "100%",
-    backgroundColor: "#6b7280",
-  },
-  headerText: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#fff",
-    textAlign: "center",
-  },
-  profileContainer: {
-    width: "100%",
-    paddingHorizontal: 24,
-    marginTop: 32,
-  },
-  profileName: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#065f46",
-  },
-  daysSavedContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    height: 200,
-    width: 200,
-    borderRadius: 100,
-    backgroundColor: "#ffdd57",
-    marginTop: 14,
-    alignSelf: "center",
-  },
-  daysSavedText: {
-    fontSize: 40,
-    fontWeight: "bold",
-    color: "#fff",
-  },
-  subtitle: {
-    marginTop: 8,
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#000",
-    textAlign: "center",
-  },
-  settingsTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#065f46",
-    marginTop: 32,
-  },
-  settingItem: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: 24,
-  },
-  settingItemText: {
-    fontSize: 18,
-    color: "#000",
-    marginLeft: 12,
-  },
-  sectionText: {
-    marginTop: 56,
-    marginLeft: 12,
-    fontSize: 18,
-    color: "#a1a1aa",
-  },
-  flexRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "100%",
-    marginLeft: 12,
-    marginTop: 28,
-  },
-  flexCol: {
-    flexDirection: "column",
-  },
-  flexColItem: {
-    marginTop: 40,
-  },
-  toggleButton: {
-    justifyContent: "center",
-    alignItems: "center",
-    paddingVertical: 4,
-    paddingHorizontal: 24,
-    marginTop: 24,
-    backgroundColor: "#4b5563",
-    borderRadius: 24,
-  },
-  toggleButtonIndicator: {
-    height: 22,
-    width: 22,
-    backgroundColor: "#fff",
-    borderRadius: 11,
-  },
-  toggleButtonText: {
-    color: "#fff",
-    marginTop: 8,
-  },
-});
+const { width, height } = Dimensions.get("window");
+const switchWidth = width * 0.15; // 15% of the screen width
+const switchHeight = switchWidth / 2; // maintain aspect ratio
+const sliderSize = switchHeight - 4; // slider size a bit smaller than the switch height
 
 const Header = ({ title }) => (
   <View style={styles.header}>
@@ -128,10 +26,7 @@ const ProfileCard = ({ imageUrl, name, daysSaved }) => (
   <View style={styles.profileContainer}>
     <Text style={styles.profileName}>{name}</Text>
     <View style={styles.daysSavedContainer}>
-      <Image
-        source={{ uri: imageUrl }}
-        style={{ ...StyleSheet.absoluteFillObject }}
-      />
+      <Image source={{ uri: imageUrl }} style={styles.profileImage} />
       <Text style={styles.daysSavedText}>{daysSaved}</Text>
     </View>
     <Text style={styles.subtitle}>Days of earth you have saved</Text>
@@ -140,94 +35,251 @@ const ProfileCard = ({ imageUrl, name, daysSaved }) => (
 
 const SettingItem = ({ iconUrl, label, onPress }) => (
   <TouchableOpacity onPress={onPress} style={styles.settingItem}>
-    <Image source={{ uri: iconUrl }} style={{ width: 40, height: 40 }} />
+    <Image source={{ uri: iconUrl }} style={styles.settingIcon} />
     <Text style={styles.settingItemText}>{label}</Text>
   </TouchableOpacity>
 );
 
-const ToggleButton = ({ label, onPress }) => (
-  <TouchableOpacity onPress={onPress} style={styles.toggleButton}>
-    <View style={styles.toggleButtonIndicator} />
-    <Text style={styles.toggleButtonText}>{label}</Text>
+const CustomSwitch = ({ value, onValueChange }) => (
+  <TouchableOpacity
+    style={[styles.switch, value ? styles.switchOn : styles.switchOff]}
+    onPress={onValueChange}
+  >
+    <View style={[styles.slider, value ? styles.sliderOn : styles.sliderOff]} />
   </TouchableOpacity>
 );
 
+const ToggleItem = ({ label, value, onToggle }) => (
+  <View style={styles.toggleItemContainer}>
+    <Text style={styles.settingItemText}>{label}</Text>
+    <CustomSwitch value={value} onValueChange={onToggle} />
+  </View>
+);
+
 function Account() {
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+  const [isPushNotifications, setIsPushNotifications] = React.useState(false);
+
   const handleEditProfile = () => Alert.alert("Edit profile clicked");
   const handleChangePassword = () => Alert.alert("Change password clicked");
-  const handleDarkModeToggle = () => Alert.alert("Dark mode toggled");
-  const handlePushNotificationsToggle = () =>
+  const handleDarkModeToggle = () => {
+    setIsDarkMode(!isDarkMode);
+    Alert.alert("Dark mode toggled");
+  };
+  const handlePushNotificationsToggle = () => {
+    setIsPushNotifications(!isPushNotifications);
     Alert.alert("Push notifications toggled");
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Header title="GroceryGrabber" />
-      <ProfileCard
-        imageUrl="https://cdn.builder.io/api/v1/image/assets/TEMP/7611a6a8b21db1ffd7d72b26deed298c206723f270e908d33d06ecc90f247e9a?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&"
-        name="Kaliraj Santosh"
-        daysSaved="330"
-      />
-      <Text style={styles.settingsTitle}>Settings</Text>
-      <SettingItem
-        iconUrl="https://cdn.builder.io/api/v1/image/assets/TEMP/31032d447a397d461dca8487d83d26125a5d62d04c8d5fe97a2b1b8ab22e231c?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&"
-        label="Kaliraj Santoshraj"
-        onPress={() => Alert.alert("Account Settings clicked")}
-      />
-      <Text style={styles.sectionText}>Account Settings</Text>
-      <View style={styles.flexRow}>
-        <View style={styles.flexCol}>
-          <TouchableOpacity onPress={handleEditProfile}>
-            <Text>Edit profile</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={handleChangePassword}
-            style={styles.flexColItem}
-          >
-            <Text>Change password</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={handleDarkModeToggle}
-            style={styles.flexColItem}
-          >
-            <Text>Dark mode</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={handlePushNotificationsToggle}
-            style={styles.flexColItem}
-          >
-            <Text>Push notifications</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.flexCol}>
-          <SettingItem
-            iconUrl="https://cdn.builder.io/api/v1/image/assets/TEMP/87873da39603045ab9bb65fffc1a0b71296ef3b05563bf7637c33a3fbe50b33f?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&"
-            label=""
-            onPress={handleDarkModeToggle}
-          />
-          <SettingItem
-            iconUrl="https://cdn.builder.io/api/v1/image/assets/TEMP/87873da39603045ab9bb65fffc1a0b71296ef3b05563bf7637c33a3fbe50b33f?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&"
-            label=""
-            onPress={handlePushNotificationsToggle}
-          />
-          <ToggleButton label="Dark mode" onPress={handleDarkModeToggle} />
-          <ToggleButton
-            label="Push notifications"
-            onPress={handlePushNotificationsToggle}
-          />
-        </View>
-      </View>
-      <View style={styles.flexRow}>
-        <Image
-          source={{
-            uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/3f08b8fe607c48a68493fcc37b78c5a7932c2656501d541bc2c568c4c62dad8e?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&",
-          }}
-          style={{ ...StyleSheet.absoluteFillObject }}
+      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+        <Header title="GroceryGrabber" />
+        <ProfileCard
+          imageUrl="https://cdn.builder.io/api/v1/image/assets/TEMP/7611a6a8b21db1ffd7d72b26deed298c206723f270e908d33d06ecc90f247e9a?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&"
+          name="Kaliraj Santosh"
+          daysSaved="330"
         />
-        <Text>Dark mode</Text>
-        <ToggleButton label="Toggle Dark mode" onPress={handleDarkModeToggle} />
-      </View>
+        <Text style={styles.settingsTitle}>Settings</Text>
+        <View style={styles.settingsContainer}>
+          <SettingItem
+            iconUrl="https://cdn.builder.io/api/v1/image/assets/TEMP/31032d447a397d461dca8487d83d26125a5d62d04c8d5fe97a2b1b8ab22e231c?apiKey=273a3e4505cd4e05ba15f44788b2ff1a&"
+            label="Kaliraj Santoshraj"
+            onPress={() => Alert.alert("Account Settings clicked")}
+          />
+          <Text style={styles.sectionText}>Account Settings</Text>
+          <View style={styles.flexCol}>
+            <TouchableOpacity
+              onPress={handleEditProfile}
+              style={styles.flexColItem}
+            >
+              <Text style={styles.settingItemText}>Edit profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={handleChangePassword}
+              style={styles.flexColItem}
+            >
+              <Text style={styles.settingItemText}>Change password</Text>
+            </TouchableOpacity>
+
+            <ToggleItem
+              label="Dark mode"
+              onToggle={handleDarkModeToggle}
+              value={isDarkMode}
+            />
+            <ToggleItem
+              label="Push notifications"
+              onToggle={handlePushNotificationsToggle}
+              value={isPushNotifications}
+            />
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  scrollViewContainer: {
+    flexGrow: 1,
+    alignItems: "center",
+    width: "100%",
+  },
+  header: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "green",
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+  },
+  profileContainer: {
+    width: "100%",
+    alignItems: "center",
+    //borderWidth: 5,
+  },
+  profileName: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#065f46",
+  },
+  daysSavedContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: width * 0.5,
+    width: width * 0.5,
+    borderRadius: width * 0.25,
+    backgroundColor: "#ffdd57",
+    marginTop: "2%",
+  },
+  profileImage: {
+    ...StyleSheet.absoluteFillObject,
+    borderRadius: width * 0.25,
+  },
+  daysSavedText: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  subtitle: {
+    marginTop: "1%",
+    alignItems: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#000",
+  },
+  settingsTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#065f46",
+    marginTop: "5%",
+    marginBottom: "2%",
+    width: "100%",
+    marginLeft: "5%",
+  },
+  settingsContainer: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2.5,
+    elevation: 5,
+    marginLeft: "2%",
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+  },
+  settingItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    marginTop: "5%",
+    marginLeft: "5%",
+  },
+  settingIcon: {
+    width: 40,
+    height: 40,
+  },
+  settingItemText: {
+    fontSize: 20,
+    color: "#000",
+    marginLeft: "5%",
+    justifyContent: "center",
+    //borderWidth: 5,
+    marginTop: "3%",
+  },
+  sectionText: {
+    marginTop: "5%",
+    marginLeft: "5%",
+    fontSize: 18,
+    color: "#a1a1aa",
+    width: "100%",
+    //borderWidth: 5,
+  },
+  flexCol: {
+    width: "100%",
+    alignItems: "center",
+    //borderWidth: 5,
+    //marginTop: 16,
+  },
+  flexColItem: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    //marginTop: 16,
+  },
+  toggleItemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    //borderWidth: 5,
+  },
+  switch: {
+    position: "relative",
+    display: "inline-block",
+    width: 60,
+    height: 32,
+    backgroundColor: "#ccc",
+    borderRadius: 34 / 2,
+    transition: ".4s",
+    padding: 2,
+  },
+  switchOn: {
+    backgroundColor: "#2196F3",
+  },
+  switchOff: {
+    backgroundColor: "#ccc",
+  },
+  slider: {
+    position: "absolute",
+    top: 1,
+    left: 1,
+    width: 30,
+    height: 30,
+    backgroundColor: "white",
+    borderRadius: 15,
+    transition: ".4s",
+  },
+  sliderOn: {
+    transform: [{ translateX: 26 }],
+  },
+  sliderOff: {
+    transform: [{ translateX: 0 }],
+  },
+});
 
 export default Account;
